ba042078ad07e3406fd11afed920f150dd6ea04c
cddcx <84568016+cddcx@users.noreply.github.com>
2023-10-28 21:57:34 +0800

    Create diy1.sh

---
+++ diys/6.1/diy1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 修改内核
+sed -i 's/PATCHVER:=*.*/PATCHVER:=6.1/g' target/linux/x86/Makefile
+
+# 修改include/target.mk
+#sed -i "s/autocore/autocore-x86/" include/target.mk
+
+# 修改target/linux/x86/Makefile
+sed -i 's/automount/autocore default-settings-chn ipset luci luci-compat luci-app-udpxy luci-app-upnp luci-app-openclash/g' target/linux/x86/Makefile
