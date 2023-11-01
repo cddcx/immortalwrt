b5b2ec0d73f2b624c1ea8b85816bf48303f91bcb
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-01 08:51:02 +0800

    Update diy1.sh

--- diys/6.1/diy1.sh
+++ diys/6.1/diy1.sh
@@ -7,7 +7,7 @@ sed -i 's/PATCHVER:=*.*/PATCHVER:=6.1/g' target/linux/x86/Makefile
 #sed -i "s/autocore/autocore-x86/" include/target.mk
 
 # 修改target/linux/x86/Makefile
-sed -i 's/automount/autocore default-settings-chn ipset luci luci-compat luci-app-udpxy luci-app-upnp luci-app-openclash/g' target/linux/x86/Makefile
+sed -i 's/automount/autocore default-settings-chn ipset luci luci-compat luci-app-udpxy luci-app-upnp/g' target/linux/x86/Makefile
 
 # luci-app-ssr-plus
 #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
