71deae8b2fabff7c2010acce72ab15e1c49ea4c1
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-01 19:33:39 +0800

    Update diy1.sh

--- diys/6.1/diy1.sh
+++ diys/6.1/diy1.sh
@@ -17,4 +17,4 @@ echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-pa
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 
 # luci-theme-kucat
-echo 'src-git luci-theme-kucat https://github.com/sirpdboy/luci-theme-kucat.git;js' >> feeds.conf.default
+echo 'src-git packages/luci-theme-kucat https://github.com/sirpdboy/luci-theme-kucat.git;js' >> feeds.conf.default
