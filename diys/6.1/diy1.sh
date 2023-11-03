63065fa1ddbe33fcf468660be975a09acbfe8438
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-03 12:41:31 +0800

    Update diy1.sh

--- diys/6.1/diy1.sh
+++ diys/6.1/diy1.sh
@@ -4,7 +4,7 @@
 sed -i 's/PATCHVER:=*.*/PATCHVER:=6.1/g' target/linux/x86/Makefile
 
 # 修改include/target.mk
-#sed -i "s/autocore/autocore-x86/" include/target.mk
+sed -i "s/kmod-nft-offload/kmod-nft-offload kmod-nft-tproxy/" include/target.mk
 
 # 修改target/linux/x86/Makefile
 sed -i 's/automount/autocore default-settings-chn ipset luci luci-compat luci-app-udpxy luci-app-upnp luci-app-v2raya/g' target/linux/x86/Makefile
