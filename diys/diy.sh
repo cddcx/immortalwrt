2ce5f6a6cbf6906623365e0224d64163632334a4
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-21 15:43:26 +0800

    Update diy.sh

--- diys/diy.sh
+++ diys/diy.sh
@@ -12,6 +12,10 @@ echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall_packages
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
 
+git clone https://github.com/yichya/luci-app-xray package/luci-app-xray
+git clone https://github.com/xiechangan123/luci-i18n-xray-zh-cn package/luci-i18n-xray-zh-cn
+git clone https://github.com/yichya/openwrt-xray package/openwrt-xray
+
 # luci-app-alist网盘管理
 git clone https://github.com/sbwml/luci-app-alist package/alist
 
