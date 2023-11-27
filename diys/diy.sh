1848fd870d46b2dd4a61047684c9242cd5fb2a11
cddcx <84568016+cddcx@users.noreply.github.com>
2023-11-27 08:20:30 +0800

    Update diy.sh

--- diys/diy.sh
+++ diys/diy.sh
@@ -25,5 +25,5 @@ git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git package/luci-th
 #sed -i '/set luci.main.mediaurlbase*/d' package/luci-theme-kucat/root/etc/uci-defaults/30_luci-kucat
 
 # SmartDNS
-git clone --depth=1 https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+#git clone --depth=1 https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+#git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
