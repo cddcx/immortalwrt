69de864f995f9a194153f8a02e512142ed656503
cddcx <84568016+cddcx@users.noreply.github.com>
2024-06-08 23:30:40 +0800

    Update diy2.sh

--- diys/diy2.sh
+++ diys/diy2.sh
@@ -88,7 +88,8 @@ sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/ut
 
 # 替换udpxy为修改版，解决组播源数据有重复数据包导致的花屏和马赛克问题
 rm -rf feeds/packages/net/udpxy/Makefile
-cp -rf ${GITHUB_WORKSPACE}/patch/udpxy/Makefile feeds/packages/net/udpxy/
+curl -sO https://https://github.com/lwb1978/OpenWrt-Actions/patch/udpxy/Makefile feeds/packages/net/udpxy/
+
 # 修改 udpxy 菜单名称为大写
 sed -i 's#\"title\": \"udpxy\"#\"title\": \"UDPXY\"#g' feeds/luci/applications/luci-app-udpxy/root/usr/share/luci/menu.d/luci-app-udpxy.json
 
