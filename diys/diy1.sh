932ed05b6f2765727d34aac413d4ff02e365d4de
cddcx <84568016+cddcx@users.noreply.github.com>
2024-08-26 16:32:54 +0800

    Update diy1.sh

--- diys/diy1.sh
+++ diys/diy1.sh
@@ -41,6 +41,13 @@ function merge_feed(){
 echo "开始 DIY1 配置……"
 echo "========================="
 
+# luci-app-homeproxy
+git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy           ####### homeproxy的默认版本(二选一) 
+#git clone -b dev https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+merge_package v5 https://github.com/sbwml/openwrt_helloworld  package/luci-app-homeproxy chinadns-ng sing-box
+sed -i "s@ImmortalWrt@OpenWrt@g" package/luci-app-homeproxy/po/zh_Hans/homeproxy.po
+sed -i "s@ImmortalWrt proxy@OpenWrt proxy@g" package/luci-app-homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
+
 ## luci-app-passwall
 #merge_package main https://github.com/xiaorouji/openwrt-passwall package luci-app-passwall
 
