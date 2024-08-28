26282b40e46646ec14cbcd9d5f0cb8491f64c4fc
cddcx <84568016+cddcx@users.noreply.github.com>
2024-08-28 22:02:18 +0800

    Update diy4.sh

--- diys/diy4.sh
+++ diys/diy4.sh
@@ -65,6 +65,9 @@ git clone https://github.com/sbwml/package_network_services_ppp package/network/
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
+# 修复编译时提示 freeswitch 缺少 libpcre 依赖
+sed -i 's/+libpcre \\$/+libpcre2 \\/g' package/feeds/telephony/freeswitch/Makefile
+
 # 替换udpxy为修改版，解决组播源数据有重复数据包导致的花屏和马赛克问题
 rm -rf feeds/packages/net/udpxy/Makefile
 cp -rf ${GITHUB_WORKSPACE}/patch/udpxy/Makefile feeds/packages/net/udpxy/
