f24bdcf503f015eb89b847a57aa8a0ab1bcadb46
cddcx <84568016+cddcx@users.noreply.github.com>
2024-10-20 16:38:23 +0800

    Update diy3.sh

--- diys/diy3.sh
+++ diys/diy3.sh
@@ -65,8 +65,8 @@ rm -rf package/network/services/ppp
 git clone https://github.com/sbwml/package_network_services_ppp package/network/services/ppp
 
 # golang 1.22
-rm -rf feeds/packages/lang/golang
-git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
 # 修复编译时提示 freeswitch 缺少 libpcre 依赖
 sed -i 's/+libpcre \\$/+libpcre2 \\/g' package/feeds/telephony/freeswitch/Makefile
