0cc216ce79b011dcc7b93218a0ba0ed117040623
cddcx <84568016+cddcx@users.noreply.github.com>
2025-05-24 16:01:15 +0800

    Update diy1.sh

--- diys/diy1.sh
+++ diys/diy1.sh
@@ -1,42 +1,4 @@
 #!/bin/bash
-#=================================================
-
-# 拉取仓库文件夹
-function merge_package() {
-	# 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
-	# 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
-	# 示例:
-	# merge_package master https://github.com/WYC-2020/openwrt-packages package/openwrt-packages luci-app-eqos luci-app-openclash luci-app-ddnsto ddnsto 
-	# merge_package master https://github.com/lisaac/luci-app-dockerman package/lean applications/luci-app-dockerman
-	if [[ $# -lt 3 ]]; then
-		echo "Syntax error: [$#] [$*]" >&2
-		return 1
-	fi
-	trap 'rm -rf "$tmpdir"' EXIT
-	branch="$1" curl="$2" target_dir="$3" && shift 3
-	rootdir="$PWD"
-	localdir="$target_dir"
-	[ -d "$localdir" ] || mkdir -p "$localdir"
-	tmpdir="$(mktemp -d)" || exit 1
-	git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
-	cd "$tmpdir"
-	git sparse-checkout init --cone
-	git sparse-checkout set "$@"
-	# 使用循环逐个移动文件夹
-	for folder in "$@"; do
-		mv -f "$folder" "$rootdir/$localdir"
-	done
-	cd "$rootdir"
-}
-
-function drop_package(){
-	find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
-}
-
-function merge_feed(){
-	./scripts/feeds update $1
-	./scripts/feeds install -a -p $1
-}
 
 echo "开始 DIY1 配置……"
 echo "========================="
@@ -52,8 +14,6 @@ sed -i "s@ImmortalWrt proxy@OpenWrt proxy@g" package/luci-app-homeproxy/htdocs/l
 #merge_package main https://github.com/xiaorouji/openwrt-passwall package luci-app-passwall
 
 # luci-app-nikki
-#merge_package main https://github.com/nikkinikki-org/OpenWrt-nikki package luci-app-nikki
-#merge_package main https://github.com/nikkinikki-org/OpenWrt-nikki package nikkio
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 
 # bpf - add host clang-15/18/20 support
