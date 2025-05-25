2b132f0d1d74cfa978f2081b04fe19b1f6eacc56
cddcx <84568016+cddcx@users.noreply.github.com>
2025-05-25 13:54:26 +0800

    Update diy3.sh

--- diys/diy3.sh
+++ diys/diy3.sh
@@ -66,8 +66,8 @@ sed -i 's/+libpcre \\$/+libpcre2 \\/g' package/feeds/telephony/freeswitch/Makefi
 # 替换udpxy为修改版，解决组播源数据有重复数据包导致的花屏和马赛克问题
 #rm -rf feeds/packages/net/udpxy/Makefile
 #curl -sfL https://raw.githubusercontent.com/lwb1978/OpenWrt-Actions/main/patch/udpxy/Makefile -o feeds/packages/net/udpxy/Makefile
-rm -rf feeds/packages/net/udpxy/Makefile
-cp -rf ${GITHUB_WORKSPACE}/patch/udpxy/Makefile feeds/packages/net/udpxy/
+#rm -rf feeds/packages/net/udpxy/Makefile
+#cp -rf ${GITHUB_WORKSPACE}/patch/udpxy/Makefile feeds/packages/net/udpxy/
 #rm -rf feeds/luci/applications/luci-app-udpxy/po
 #cp -rf ${GITHUB_WORKSPACE}/patch/luci-app-udpxy/po feeds/luci/applications/luci-app-udpxy/po
 
